0e7c51c083111f78faa420d0ef6aad5765fee690
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-04-05 12:30:21 +1100

    init commit

--- fitness-tracker-api/server.js
+++ fitness-tracker-api/server.js
@@ -1,7 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-require('dotenv').config();
-const workoutRoutes = reuired('./workouts.js'); 
+import express, { json } from "express";
+import cors from "cors";
+//require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,12 +8,15 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
 
-app.use("/workouts", workoutRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Fitness Tracker API is running!');
+app.get("/", (req, res) => {
+    res.send("Fitness Tracker API is running!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong!" });
 });
